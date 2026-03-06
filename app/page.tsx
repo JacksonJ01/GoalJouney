e297467fc1ec31ef8a46cd66b6beb4d34a0451fd
JacksonJ01 <jaredajackson01@gmail.com>
2026-03-05 23:13:22 -0800

    modifying page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
-import { supabase } from "@/lib/supabase";
-
-export default async function Home() {
-  // const { data, error } = await supabase
-  //   .from("profiles")
-  //   .select("*");
-
-  // console.log("DATA:", data);
-  // console.log("ERROR:", error);
-
+// app/page.tsx
+export default function Home() {
   return (
     <div className="p-10">
       <h1>Supabase Test</h1>
-      <p>Check your terminal for output.</p>
+      <p>
+        Homepage is live and safe for deployment. Backend will be added later.
+      </p>
     </div>
   );
 }
